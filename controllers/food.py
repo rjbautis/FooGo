10afb22bdf060294a33c394cf7ed6aef2acbbc06
Kai Huang <kaihuang9397@gmail.com>
2018-05-27 16:32:32 -0700

    fixed burger redirect

---
+++ controllers/food.py
@@ -0,0 +1,44 @@
+# food.py controller
+
+import json
+
+
+def breakfast():
+    return dict(message=T('Welcome to web2py!'))
+
+
+def burgers():
+    return dict(message=T('Welcome to web2py!'))
+
+
+def desserts():
+    return dict(message=T('Welcome to web2py!'))
+
+
+def healthy():
+    return dict(message=T('Welcome to web2py!'))
+
+
+def korean():
+    return dict(message=T('Welcome to web2py!'))
+
+
+def pasta():
+    return dict(message=T('Welcome to web2py!'))
+
+
+def pizza():
+    return dict(message=T('Welcome to web2py!'))
+
+
+def sandwiches():
+    return dict(message=T('Welcome to web2py!'))
+
+
+def sushi():
+    return dict(message=T('Welcome to web2py!'))
+
+
+def burger():
+    return dict(message=T('Welcome to web2py!'))
+
